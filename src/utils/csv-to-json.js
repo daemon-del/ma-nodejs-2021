f0bb26b243ef98826a89710d698c94a15ca3386a
daemon-del <khotaru.kreyn@mail.ru>
2020-12-11 20:21:27 +0200

    fixed csv to json

--- src/utils/csv-to-json.js
+++ src/utils/csv-to-json.js
@@ -1,20 +1,62 @@
 const { Transform } = require('stream');
 
-
 function createCsvToJson() {
+  let isNotFirst = false;
+  let insertLineSeparator = false;
+  let columnsHeaders = [];
+  let rowCount = 0;
+
   const transform = (chunk, encoding, callback) => {
-    const string = chunk.toString();
-    callback(null, string);
+    let result = '';
+
+    const stringProduct = chunk.toString('utf8');
+    if (columnsHeaders.toString() === stringProduct) return callback(null, '');
+
+    const arrayProduct = stringProduct.split(',');
+
+    const addAllColumnsInProduct = arrayValuesProduct => {
+      const product = {};
+      arrayValuesProduct.forEach((valueProduct, index) => {
+        product[columnsHeaders[index]] = valueProduct;
+      });
+
+      const fullProduct = {
+        type: product.type,
+        color: product.color,
+        quantity: product.quantity,
+      };
+
+      if (product.isPair === 'true') fullProduct.priceForPair = product.price;
+      else fullProduct.price = product.price;
+
+      return fullProduct;
+    };
+
+    if (isNotFirst) {
+      rowCount += 1;
+
+      if (insertLineSeparator) result += ',\n';
+      else insertLineSeparator = true;
+
+      const product = addAllColumnsInProduct(arrayProduct);
+      result += `${JSON.stringify(product)}`;
+    } else {
+      result += '[';
+      isNotFirst = true;
+      columnsHeaders = arrayProduct;
+    }
+
+    return callback(null, result);
   };
 
   const flush = callback => {
-    console.log('No more data to read.');
-    callback(null);
+    console.log(`Parsed ${rowCount} rows at all!`);
+    callback(null, ']');
   };
 
   return new Transform({ transform, flush });
 }
 
 module.exports = {
-  createCsvToJson
+  createCsvToJson,
 };
